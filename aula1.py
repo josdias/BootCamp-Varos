4a8c7c4873f2065f0000ebdf9eaa4c0afff9d138
José Dias <devjosedias@gmail.com>
2023-04-04 20:50:47 -0300

    Primeiro Commit

---
+++ aula1.py
@@ -0,0 +1,10 @@
+import panda as pd
+import datetime
+import yfinance as yf
+from matplotlib import pyplot as plt
+import mplcyberpunk
+import smtplib
+from email.message import EmailMessage
+
+nome_programador = "Paulo"
+ativos = ["^BVSP", "BRL=X"]
